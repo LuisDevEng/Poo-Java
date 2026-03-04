6b55e20b885d6cd4fa49137fbde613d7b924d593
LuisDevEng <gerutionpower99@gmail.com>
2026-03-04 13:47:36 -0300

    Re

--- Carros/src/Main.java
+++ Carros/src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Carro carro1 = new Carro("Fiatinho","ARO129",100);
+        Carro carro1 = new Carro("Fiat","ARO129",100);
         Cliente cliente1 = new Cliente("Luis","1231412");
         Locacao locacao1 = new Locacao(cliente1,carro1,12);
         locacao1.exibirResumo();
